aef1f5d8cb398fcfbabc7e66c07436e060f703df
zanglang <zanglang@121455e0-47f1-4703-9c30-fe25dbf06416>
2009-03-21 11:27:49 +0000

    Initial commit

---
+++ RollTracker.lua
@@ -0,0 +1,78 @@
+local rollArray
+local rollNames
+
+function RollTracker_OnLoad()
+	rollArray = {}
+	rollNames = {}
+	this:RegisterEvent("CHAT_MSG_SYSTEM")
+	
+	-- slash command
+	SLASH_ROLLTRACKER1 = "/rolltracker";
+	SLASH_ROLLTRACKER2 = "/rt";
+	SlashCmdList["ROLLTRACKER"] = function (msg)
+		if msg == "clear" then
+			RollTracker_ClearRolls()
+		else
+			RollTracker_ShowWindow()
+		end
+	end
+end
+
+-- Rolling function
+function RollTracker_CHAT_MSG_SYSTEM(msg)
+	local name, roll, low, high
+	
+	for name, roll, low, high in string.gmatch(arg1, "([^%s]+) rolls (%d+) %((%d+)%-(%d+)%)$") do
+		-- check for rerolls. >1 if rolled before
+		rollNames[name] = rollNames[name] and rollNames[name] + 1 or 1
+		table.insert(rollArray, {
+			Name = name,
+			Roll = tonumber(roll),
+			Low = tonumber(low),
+			High = tonumber(high),
+			Count = rollNames[name]
+		})
+		RollTracker_ShowWindow()
+	end
+end
+
+-- Sort and format the list
+function RollTracker_UpdateList()
+	local i
+	local rollText = ""
+	
+	table.sort(rollArray, function (a, b)
+		return a.Roll < b.Roll
+	end)
+	
+	-- format and print rolls, check for ties
+	for i, roll in pairs(rollArray) do
+		local tied = (rollArray[i + 1] and roll.Roll == rollArray[i + 1].Roll) or (rollArray[i - 1] and roll.Roll == rollArray[i - 1].Roll)
+		rollText = string.format("|c%s%d|r: |c%s%s%s%s|r\n",
+				tied and "ffffff00" or "ffffffff",
+				roll.Roll,
+				((roll.Low ~= 1 or roll.High ~= 100) or (roll.Count > 1)) and  "ffffcccc" or "ffffffff",
+				roll.Name,
+				(roll.Low ~= 1 or roll.High ~= 100) and format(" (%d-%d)", roll.Low, roll.High) or "",
+				roll.Count > 1 and format(" [%d]", roll.Count) or "") .. rollText
+	end
+	RollTrackerRollText:SetText(rollText)
+	RollTrackerFrameStatusText:SetText(string.format("%d Roll(s)", table.getn(rollArray)))
+end
+
+function RollTracker_ClearRolls()
+	rollArray = {}
+	rollNames = {}
+	DEFAULT_CHAT_FRAME:AddMessage("All rolls have been cleared.")
+	RollTracker_UpdateList()
+end
+
+--	GUI Functions
+function RollTracker_ShowWindow()
+	RollTrackerFrame:Show()
+	RollTracker_UpdateList()
+end
+
+function RollTracker_HideWindow()
+	RollTrackerFrame:Hide()
+end
